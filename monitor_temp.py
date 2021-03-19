6bafede269f964e202e6e55eda0e3fa509bd7209
Ryley <ryley.g@hotmail.com>
2021-03-19 10:03:41 -0600

    cast dict_values to list in temp_log write operation

--- monitor_temp.py
+++ monitor_temp.py
@@ -5,7 +5,7 @@ import sys
 import TempMonitor
 from os import getenv
 import dotenv
-from datetime import datetime
+
 
 dotenv.load_dotenv()
 
@@ -15,24 +15,24 @@ PROVINCE = getenv('PROVINCE')
 COUNTRY = getenv('COUNTRY')
 
 if __name__ == '__main__':
-        m = TempMonitor.TempMonitor(CITY, PROVINCE, COUNTRY, API_KEY)
-        # TODO: UPDATE THIS TO TAKE ARGUMENT FLAGS FOR TIME, MEASUREMENTS
-        try:
-            num_measurements = int(sys.argv[1])
-        except IndexError:
-            num_measurements = 40
-        try:
-            for _ in range(num_measurements):
-                    m.get_temps()
-                    sleep(1800)
-        except KeyboardInterrupt:
-            with open('temp_log.txt', 'w') as f:
-                for i in range(len(m.cpu_temps.values())):
-                    temp = m.cpu_temps.values()[0][i]
-                    time_dt = m.cpu_temps.values()[1][i]
-                    time_str = time_dt.datetime.strftime("%D/%M/%Y %H:%M:%S")
-                    f.write(f'Time: {time_str}, temp: {temp}')
-            print('Program exited by keyboard interrupt')
-            m.line_plot()
-            raise SystemExit
+    m = TempMonitor.TempMonitor(CITY, PROVINCE, COUNTRY, API_KEY)
+    # TODO: UPDATE THIS TO TAKE ARGUMENT FLAGS FOR TIME, MEASUREMENTS
+    try:
+        num_measurements = int(sys.argv[1])
+    except IndexError:
+        num_measurements = 40
+    try:
+        for _ in range(num_measurements):
+            m.get_temps()
+            sleep(1800)
+    except KeyboardInterrupt:
+        with open('temp_log.txt', 'w') as f:
+            for i in range(len(m.cpu_temps.values())):
+                temp = list(m.cpu_temps.values())[0][i]
+                time_dt = list(m.cpu_temps.values())[1][i]
+                time_str = time_dt.datetime.strftime("%D/%M/%Y %H:%M:%S")
+                f.write(f'Time: {time_str}, temp: {temp}')
+        print('Program exited by keyboard interrupt')
+        m.line_plot()
+        raise SystemExit
 
