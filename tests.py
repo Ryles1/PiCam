23ac823401f071c6f41d0c493413daef2b791a9c
Ryley <ryley.g@hotmail.com>
2021-03-23 21:38:13 -0600

    added keyboard exit

--- tests.py
+++ tests.py
@@ -2,6 +2,8 @@ import unittest
 from TempMonitor import TempMonitor
 import dotenv
 from os import getenv
+import datetime
+import matplotlib.pyplot as plt
 
 dotenv.load_dotenv()
 API_KEY = getenv('WEATHER_API')
@@ -11,7 +13,12 @@ COUNTRY = getenv('COUNTRY')
 
 class TestTempMonitor(unittest.TestCase):
     def testplot(self):
+        m = TempMonitor(CITY, PROVINCE, COUNTRY, API_KEY)
+        temps = [1, 2, 3, 4, 5]
+        delta = datetime.timedelta(minutes=15)
+        times = [datetime.datetime.now() + i * delta for i in range(5)]
         pass
+
 
     def test_ambient(self):
         m = TempMonitor(CITY, PROVINCE, COUNTRY, API_KEY)
